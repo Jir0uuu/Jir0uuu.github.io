8e1c982b4d5f7aca77981397a441007219870626
Jir0uuu <domingodanilo123@gmail.com>
2023-03-31 14:26:41 +0800

    initial commit

---
+++ Javascript.js
@@ -0,0 +1,7 @@
+let typingEffect = new Typed("#multiTexts", {
+    strings : ["(Portfolio)" ,"(FrontEnd)" , "(Backend)", "(Data Management)"], 
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+})
